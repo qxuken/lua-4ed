3d6dce1dfce6ad885e2dc93a79d5c9b1efaaf571
Viktor Nasonov <qxuken@gmail.com>
2023-11-19 18:38:18 +0300

    Restructured folders and added permutations algo

--- functions.lua
+++ functions.lua
@@ -1,17 +1,48 @@
 function prints_all(arr)
   for i = 1, #arr do
-    print(arr[i])
+    io.write(arr[i])
+    if i ~= #arr then
+      io.write(" ")
+    end
   end
+  io.write("\n")
 end
 
-
-function stip_first(_first, ...)
+function strip_first(_, ...)
   return ...
 end
 
-function stip_last(...)
+function strip_last(...)
   local args = table.pack(...)
-  prints_all(args)
   return table.unpack(args, 1, #args - 1)
 end
 
+function shuffle(arr)
+  for i = #arr, 2, -1 do
+    local j = math.random(i)
+    arr[i], arr[j] = arr[j], arr[i]
+  end
+end
+
+function swap(s, i, j)
+  if i == j then
+    return s
+  end
+  return s:sub(1, i - 1) .. s:sub(j, j) .. s:sub(i + 1, j - 1) .. s:sub(i, i) .. s:sub(j + 1)
+end
+
+function print_all_combinations(arr)
+  local str = table.concat(arr);
+  local function print_combinations(s, l)
+    if l == #s then
+      print(s)
+      return
+    end
+    for i = l, #s do
+      local new_str = swap(s, l, i)
+      print_combinations(new_str, l + 1)
+    end
+  end
+
+  print_combinations(str, 1)
+end
